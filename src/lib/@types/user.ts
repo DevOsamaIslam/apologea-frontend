4571e07fe769aedbeb903b53ce569cb8e38f201c
Osama <Osamah.a@mblhightech.com>
2022-10-15 15:02:05 +0300

    remove unused interfaces

--- src/lib/@types/user.ts
+++ src/lib/@types/user.ts
@@ -1,44 +1,5 @@
 import { ILoginResponse } from './auth/login'
 
-export interface IEntities {
-	accessToken: string
-	createdAt: string
-	createdBy: string
-	deletedAt: null
-	deletedBy: string
-	email: string
-	firstName: string
-	id: string
-	ipWhiteList: string[]
-	isTwoFactorAuthEnabled: string
-	language: string
-	lastName: string
-	lastPasswordChange: string | null
-	roleId: string
-	updatedAt: string
-	updatedBy: string
-	username: string
-	role: {
-		id: string
-		name: string
-	}
-}
-
-export interface IUserState {
-	entities: IEntities | undefined
-	loading: boolean
-	authorized: boolean
-	accessToken: string | undefined
-	forgotPassword: {
-		loading: boolean
-		msg: { message: string } | undefined
-	}
-	resetPassword: {
-		loading: boolean
-		msg: string | undefined
-	}
-}
-
 export interface IUser extends ILoginResponse {
 	id: string
 	accessToken: string
